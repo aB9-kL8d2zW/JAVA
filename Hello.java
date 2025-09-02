7e184d116c78e27e1488180120df78f273621ec1
Rohith003363 <rr9991043@gmail.com>
2025-09-02 17:17:03 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+package javaday2;
+
+public class Hello {
+
+    void helloWorld() {
+        System.out.println("Hello world");
+    }
+
+    public static void main(String[] args) {
+      
+        Hello hello = new Hello();
+        
+        hello.helloWorld();
+        hello.helloWorld();
+    }
+}
