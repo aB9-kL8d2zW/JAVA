a5a7c18ca8e9c8d2a5ce1215e3a6f0554aae7350
Rohith003363 <rr9991043@gmail.com>
2025-09-02 17:07:36 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,13 @@
+package javaday2;
+
+public class Demo {
+	public static void main(String[]args) {
+		System.out.println(10);
+		System.out.println(10+20+"VALUE IS ");
+		System.out.println(10+20+"Value is " +10);
+		System.out.println(50+30+"VALUE IS :"+10+20);
+		System.out.println(56+23+"VALUE IS :"+(10+20));
+		
+	}
+
+}
